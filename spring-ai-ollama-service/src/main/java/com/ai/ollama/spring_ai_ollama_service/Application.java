171e06e2fed50a9909c7f91cd5e81c1b399bbbde
naidusrikanth <naidusrikanth7@gmail.com>
2025-10-02 08:08:25 +0530

    ollama ai service demo project

---
+++ spring-ai-ollama-service/src/main/java/com/ai/ollama/spring_ai_ollama_service/Application.java
@@ -0,0 +1,13 @@
+package com.ai.ollama.spring_ai_ollama_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
