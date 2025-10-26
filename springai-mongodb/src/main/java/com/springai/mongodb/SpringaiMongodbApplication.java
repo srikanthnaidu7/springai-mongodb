40f63663e3d6761e842515ab8a231fdb7915b658
naidusrikanth <naidusrikanth7@gmail.com>
2025-10-26 08:29:52 +0530

    using mcp spring mcp with mongodb

---
+++ springai-mongodb/src/main/java/com/springai/mongodb/SpringaiMongodbApplication.java
@@ -0,0 +1,24 @@
+package com.springai.mongodb;
+
+import com.springai.mongodb.tools.EmployeeMCPService;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringaiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiMongodbApplication.class, args);
+	}
+
+    @Bean
+    public List<ToolCallback> employeeToolCallback(EmployeeMCPService employeeMCPService){
+        return List.of(ToolCallbacks.from(employeeMCPService));
+    }
+
+}
