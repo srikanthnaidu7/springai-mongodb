60090255c02344b361bc59d1cb6b0f37b32893d7
naidusrikanth <naidusrikanth7@gmail.com>
2025-10-26 08:34:42 +0530

    test commit

--- springai-mongodb/src/main/java/com/springai/mongodb/SpringaiMongodbApplication.java
+++ springai-mongodb/src/main/java/com/springai/mongodb/SpringaiMongodbApplication.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpringaiMongodbApplication {
-
+//Test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringaiMongodbApplication.class, args);
 	}
